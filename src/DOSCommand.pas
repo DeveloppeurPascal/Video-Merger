51356c69ccdf72de298a398edc23febca32012bd
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-21 10:58:17 +0200

    added copyright and timestamp comments

--- src/DOSCommand.pas
+++ src/DOSCommand.pas
@@ -1,3 +1,35 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  Video Merger
+
+  Copyright 2024-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://videomerger.olfsoftware.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Video-Merger
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:43:14.896+02:00
+  Signature : 9d634cbf7eab043503f9f0b903067f0dae76cd40
+  ***************************************************************************
+*)
+
 {$IFDEF MSWINDOWS}
 {$I '..\lib-externes\DOSCommand\Source\DosCommand.pas'}
 {$ELSE}
--- src/uMergingWorker.pas
+++ src/uMergingWorker.pas
@@ -1,3 +1,35 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  Video Merger
+
+  Copyright 2024-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://videomerger.olfsoftware.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Video-Merger
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:43:14.904+02:00
+  Signature : b3556eee95f6fd5c7e0e0a4411114c82de83dcf8
+  ***************************************************************************
+*)
+
 unit uMergingWorker;
 
 interface
